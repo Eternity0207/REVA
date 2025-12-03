5fb4b7ecae5decc389cb68277367e271a188abd4
MANI <arshgoyal.67@gmail.com>
2025-12-03 11:08:28 +0530

    Add prompt_toolkit integration

--- operate/utils/style.py
+++ operate/utils/style.py
@@ -1,4 +1,5 @@
 """Terminal styling"""
+from prompt_toolkit.styles import Style
 
 ANSI_GREEN = "\033[92m"
 ANSI_RED = "\033[91m"
@@ -6,3 +7,5 @@ ANSI_YELLOW = "\033[93m"
 ANSI_BLUE = "\033[94m"
 ANSI_BRIGHT_MAGENTA = "\033[95m"
 ANSI_RESET = "\033[0m"
+
+style = Style.from_dict({"prompt": "blue bold"})
