d62b07a898d2204c89845ef6738789560304c5aa
MANI <arshgoyal.67@gmail.com>
2025-12-21 09:57:58 +0530

    Create misc utilities module

--- operate/utils/misc.py
+++ operate/utils/misc.py
@@ -1,5 +1,4 @@
 """Miscellaneous utilities"""
-import os
 
 def convert_percent_to_decimal(value):
     """Convert value to decimal"""
@@ -7,8 +6,3 @@ def convert_percent_to_decimal(value):
         return float(value)
     except (ValueError, TypeError):
         return None
-
-def ensure_display():
-    """Ensure display is available"""
-    if not os.environ.get("DISPLAY"):
-        os.environ["DISPLAY"] = ":0"
